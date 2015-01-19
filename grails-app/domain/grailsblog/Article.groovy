354d2b29b65e6ae8d0eed7d77bfc184438fbe321
aksommerville <aksommerville@gmail.com>
2015-01-19 15:07:24 -0500

    Article and User domain classes.

--- grails-app/domain/grailsblog/Article.groovy
+++ grails-app/domain/grailsblog/Article.groovy
@@ -2,6 +2,17 @@ package grailsblog
 
 class Article {
 
+    User user;
+    String title;
+    String content;
+
     static constraints = {
+      title()
+      user(blank:false)
+      content()
+    }
+    
+    static mapping={
+      stuff column:'stuff', type:'text'
     }
 }
--- grails-app/domain/grailsblog/User.groovy
+++ grails-app/domain/grailsblog/User.groovy
@@ -2,6 +2,18 @@ package grailsblog
 
 class User {
 
+    String name;
+    String email;
+    String password;
+    
+    static hasMany=[
+      articles:Article,
+      comments:Comment,
+    ];
+
     static constraints = {
+      name(size:1..20,unique:true,blank:false)
+      email(email:true,blank:false)
+      password(size:6..20,blank:false)
     }
 }
